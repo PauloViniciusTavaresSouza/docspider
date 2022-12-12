9ce705f15ab23cdd2810330cf59abcfc11ff3684
Paulo Vinicius <pauloviniciustavaressouza@gmail.com>
2022-12-12 20:58:20 -0300

    feat: create fake DBA

---
+++ src/Database/FakeDatabase.js
@@ -0,0 +1,10 @@
+export const fakeData = [
+  {
+    title: 'Avaliação de negócios',
+    description:
+      'Encaminando um arquivo onde mostrará todas as minhas regras de negocios',
+    uploadFile: [
+      { name: 'paulo', type: 'Qualquer coisa', dateTime: 'Brasilia' },
+    ],
+  },
+];
--- src/Pages/Documents.js
+++ src/Pages/Documents.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { HeaderDoc } from '../Components/HeaderDocumentos/HeaderDoc';
-import styles from './Documents.module.css';
-import FileDownload from 'js-file-download';
-import { Axios } from 'axios';
+import React, { useEffect } from 'react';
+import { fakeData } from '../Database/FakeDatabase';
+import Doc from '../img/doc.png';
 
-export const Documents = ({ open }) => {
-  const download = (e) => {
-    e.preventDefault();
-    Axios({
-      ulr: 'http://localhost:8080/',
-      method: 'GET',
-      responseType: 'blob',
-    }).then((res) => {
-      FileDownload(res.data, 'Donwloaded.png');
-    });
-  };
+import styles from './Documents.module.css';
+
+export const Documents = () => {
+  const [data, setData] = React.useState([]);
+  useEffect(() => {
+    setData(fakeData);
+  });
+
   return (
     <section className={styles.containerPrincipal}>
-      <button onClick={(e) => download(e)}>Download</button>
+      <div className={styles.pageDoc}>
+        <h1 className={styles.TitleDoc}>Meus Documentos</h1>
+        {data &&
+          data.map((item) => {
+            return (
+              <div key={item} className={styles.cardDoc}>
+                <img src={Doc} alt="icon documento" />
+                <h2>{item.title}</h2>
+                <p>Descrição: {item.description}</p>
+                <p>{item.uploadFile[0].name}</p>
+                <p>{item.uploadFile[0].type}</p>
+                <div>{item.uploadFile[0].dateTime.constructor()}</div>
+              </div>
+            );
+          })}
+      </div>
     </section>
   );
 };
